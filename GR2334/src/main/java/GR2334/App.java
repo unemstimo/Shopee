67f64b471b9de3fbd00013178fc8479b4e2e50d5
Oskar Voldsund <oskarvoldsund@dhcp-10-22-29-192.wlan.ntnu.no>
2023-09-12 15:25:47 +0200

    Added maven to the project

---
+++ GR2334/src/main/java/GR2334/App.java
@@ -0,0 +1,13 @@
+package GR2334;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GR2334/src/test/java/GR2334/AppTest.java
@@ -0,0 +1,20 @@
+package GR2334;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
