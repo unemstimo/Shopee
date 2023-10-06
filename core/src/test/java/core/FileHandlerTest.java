078651b6cfcb7bb5856a184e429b477a62dbad78
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-10-06 22:35:43 +0200

    Added test for updating list  info and password

--- core/src/test/java/core/FileHandlerTest.java
+++ core/src/test/java/core/FileHandlerTest.java
@@ -52,5 +52,34 @@ public class FileHandlerTest {
         assertEquals("Chips", testUser.getShopeeList().getBoughtItem(0).getFoodName());
     }
 
-   
+    @Test
+    public void testUpdateUserInFile() {
+        fileHandler.clearFileContent();
+        // Create a User object for testing
+        User testUser = new User("oskar@gmail.com", "Password12!");
+        User testUser2 = new User("Hanna@gmail.com", "Henryd12!");
+        ShopeeList newList = new ShopeeList("Onsdag");
+        newList.addFoodShopList("Chips",7);
+        newList.addFoodShopList("Chips",3);
+        testUser.setShopeeList(newList);
+
+        // Write the User object to the test file
+        fileHandler.writeToFile(testUser);
+        fileHandler.writeToFile(testUser2);
+
+        // Update the User object
+        testUser.setPassword("NewPwd4$");
+        fileHandler.writeToFile(testUser);
+
+        // Read the User object from the test file
+        List<User> userList = fileHandler.JsonToObj();
+
+        // Assert that the User object was successfully updated
+        assertNotNull(userList);
+        assertEquals(2, userList.size());
+        assertEquals("NewPwd4$", userList.get(0).getPassword());
+        System.out.println(newList.getShopList());
+        assertEquals(3, testUser.getShopeeList().getShopList().get(0).getFoodAmount());
+    }
+
 }
