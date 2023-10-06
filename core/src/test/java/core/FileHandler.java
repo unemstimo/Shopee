76e19d07c8f297b7dafb13a4eb0477a548205aa8
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-10-06 14:07:41 +0200

    created test class FileHandlerTest.java to implement testing functionality for FileHandler

---
+++ core/src/test/java/core/FileHandler.java
@@ -0,0 +1,11 @@
+package core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import core.Storage.FileHandeler;
+
+public class FileHandler {
+    
+}
