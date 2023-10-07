217a88d93da87b6f3bfb8e27188a11a9aa902d67
Une Marie Stimo <une@stimo.no>
2023-10-07 17:12:12 +0200

    Removed unused imports in ShopeeTest.java

--- ui/src/test/java/ui/ShopeeTest.java
+++ ui/src/test/java/ui/ShopeeTest.java
@@ -3,7 +3,7 @@ package ui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
@@ -13,17 +13,10 @@ import static org.testfx.matcher.base.NodeMatchers.isVisible;
 import static org.testfx.api.FxAssert.verifyThat;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.matcher.control.LabeledMatchers;
-
 import core.FoodItem;
 
 /**
@@ -31,14 +24,13 @@ import core.FoodItem;
  */
 public class ShopeeTest extends ApplicationTest {
 
-    private ShopeeController controller;
+    
     private Parent root;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Shopee.fxml"));
         root = fxmlLoader.load();
-        controller = fxmlLoader.getController();
         stage.setScene(new Scene(root));
         stage.show();
     }
