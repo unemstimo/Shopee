d656504dedcd74c3d2ced82350369081b722b19d
hannakongelf <122216425+hannakongelf@users.noreply.github.com>
2023-10-23 10:44:40 +0200

    Deleted old folder structure. Added som new folders. Updated Module-Info.java files and pom.xml files. Issue #155. Co-authored-by: Une Marie Stimo <unems@stud.ntnu.no>

--- shopee/ui/src/main/java/module-info.java
+++ shopee/ui/src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module ui {
     requires com.fasterxml.jackson.databind;
     
-    requires core;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
+    requires transitive core;
+
+    exports shopee.ui;
+    exports shopee.ui.dataaccess; // Mappen må fylles med filer for at denne streken skal gå bort. Det blir gjort i et annet issue. 
 
     opens shopee.ui to javafx.graphics, javafx.fxml;
 }
