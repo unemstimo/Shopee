be72426b429a143da14f0d90d8fb9dcc6064e625
hannakongelf <122216425+hannakongelf@users.noreply.github.com>
2023-10-24 15:59:02 +0200

    Saving todays work on the LocalUserAccess.java

--- shopee/ui/src/main/java/shopee/ui/dataaccess/LocalUserAccess.java
+++ shopee/ui/src/main/java/shopee/ui/dataaccess/LocalUserAccess.java
@@ -1,20 +1,61 @@
 package shopee.ui.dataaccess;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
 import shopee.core.ShopeeList;
 import shopee.core.User;
+import shopee.json.FileHandeler;
+
+/**
+ * Sets the persistence storage file when using the application locally.
+ */
 
 public class LocalUserAccess implements UserAccess {
 
+    private final FileHandeler persistence = new FileHandeler();
+    private User user = new User();
+
+    /**
+     * Constructor to initialize persistence
+     */
+    public LocalUserAccess() {
+        persistence.setSaveFilePath("Shopee.json");
+        try {
+        this.user = persistence.loadUser();
+        } catch (IllegalStateException | IOException e) {
+        this.user = new User();
+        }
+    }
+
+    public LocalUserAccess(String path) {
+        persistence.setSaveFilePath(path);
+        try {
+        this.user = persistence.loadUser();
+        } catch (IllegalStateException | IOException e) {
+        this.user = new User();
+        } 
+    }
+
     @Override
     public User loadUser(String username, String password) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUser'");
+        try {
+            user.setUsername(username);
+            user.setPassword(password);
+            return user;
+          } 
+          catch (IllegalStateException | NoSuchElementException e) {
+            user.setUsername(username);
+            user.setPassword(password);
+            return user;
+        }
     }
 
     @Override
     public boolean editShopeeList(String listName, ShopeeList editedList) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'editShopeeList'");
+        ShopeeList existingList = user.getShopeeList(listName);
+        existingList.setShopList(editedList.getShopList());
+        existingList.setBoughtList(editedList.getBoughtList());
     }
 
     @Override
