8488d9a25fae4ad773905c9d101784a2f913b9bb
Une Marie Stimo <une@stimo.no>
2023-10-24 10:57:18 +0200

    Implemented entry point for spring boot in ShopeeAppApplication.java in issue 148

---
+++ shopee/rest/src/main/java/shopee/rest/ShopeeAppApplication.java
@@ -0,0 +1,18 @@
+package rest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/* 
+The Spring Application
+ */
+@SpringBootApplication
+public class ShopeeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeeAppApplication.class, args);
+    }
+
+}
