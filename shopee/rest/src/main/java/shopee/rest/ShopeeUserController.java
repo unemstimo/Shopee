cbadc6d39696d4bd82f85880a8b1bfb2ba0c9dcc
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-05 13:52:47 +0100

    Implemented a controller in rest
    Co-authored-by: Oskar Voldsund <oskarvoldsund@users.noreply.github.com>

--- shopee/rest/src/main/java/shopee/rest/ShopeeUserController.java
+++ shopee/rest/src/main/java/shopee/rest/ShopeeUserController.java
@@ -1,14 +1,13 @@
 package shopee.rest;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import shopee.core.User;
 import shopee.core.ShopeeList;
-import shopee.core.FoodItem;
 
 /**
  * Class responsible for handling following requests through the REST API:
@@ -26,18 +24,5 @@ import shopee.core.FoodItem;
 @RequestMapping("/users")
 
 public class ShopeeUserController {
-  @Autowired
-  private ShopeeUserService userService;
-
-  public ShopeeUserController(final ShopeeUserService userService) throws IllegalStateException, IOException {
-      this.userService = new ShopeeUserService();
-  }
-
-  @Bean
-  public ShopeeUserService listContainerBean()  {
-    return new ShopeeUserService();
-  }
-
-    
-    
+  
 }
