f56a566d0d1eb70badc88470f30c1f99e0959870
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-05 13:29:03 +0100

    implemented the application for rest folder
    Co-authored-by: oskarvo <oskarvo@stud.ntnu.no>

--- shopee/rest/src/main/java/shopee/rest/ShopeeAppApplication.java
+++ shopee/rest/src/main/java/shopee/rest/ShopeeAppApplication.java
@@ -1,8 +1,7 @@
-package rest;
+package shopee.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /* 
 The Spring Application
