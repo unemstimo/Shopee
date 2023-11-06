ce31f890ca14da68c5297e92552c80d0047a2c48
Oskar Voldsund <osk.voldsund@gmail.com>
2023-11-06 15:22:21 +0100

    Set up initialization shopeeTest

--- shopee/ui/src/test/java/shopee/ui/ShopeeTest.java
+++ shopee/ui/src/test/java/shopee/ui/ShopeeTest.java
@@ -10,11 +10,15 @@ import shopee.core.FoodItem;
 import shopee.core.ShopeeList;
 import shopee.core.User;
 import shopee.json.FileHandeler;
+import shopee.ui.dataaccess.LocalUserAccess;
+import shopee.ui.dataaccess.UserAccess;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
@@ -24,22 +28,40 @@ import org.testfx.framework.junit5.ApplicationTest;
 public class ShopeeTest extends ApplicationTest {
 
     
-    private Parent root;
-    private User testUser;
+    private User testUser = new User("Oskar@ntnu.no", "Eksempelpassors123@");
+    private FileHandeler fileHandeler = new FileHandeler("direct.json");
+
+
+    private ShopeeController controller = new ShopeeController();
+    private UserAccess dataAccess;
+  
+    
+  
+    // @BeforeAll
+    // public void rigup() throws FileNotFoundException {
+    //     this.testUser = exampleUser();
+    // }
+  
+    @BeforeEach
+    public void setUp() throws FileNotFoundException {
+      this.dataAccess = new LocalUserAccess();
+      fileHandeler.clearFileContent();
+      this.testUser.addShopeeList(new ShopeeList("testlist"));
+      controller.initData(this.testUser, "testlist", this.dataAccess);  
+  
+    }
+
+
+
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Shopee.fxml"));
-        root = fxmlLoader.load();
-        this.testUser = exampleUser();
-        ShopeeController controller = fxmlLoader.getController();
-        controller.setUser(testUser);
-        stage.setScene(new Scene(root));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setController(controller);
+        fxmlLoader.setLocation(this.getClass().getResource("Home.fxml"));
+        final Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
         stage.show();
-    }
-
-    public Parent getRootNode() {
-        return root;
     }
 
     /**
@@ -57,10 +79,13 @@ public class ShopeeTest extends ApplicationTest {
 
         assertEquals("", lookup("#newFood").queryTextInputControl().getText());
         assertEquals("", lookup("#amountNewFood").queryTextInputControl().getText());
-        ShopeeList s = testUser.getShopeeList();
-        FoodItem item = new FoodItem("Pineapple", 2);
-        assertEquals(2, s.getShopList().size());
-        assertEquals(item.getFoodName(), s.getShopList().get(1).getFoodName());
+
+        ShopeeList shopelist = this.dataAccess.getAllUsers().get(0).getShopeeList("testlist");
+        FoodItem item = shopelist.getFood("Pineapple");
+        int amount = item.getFoodAmount();
+        
+        assertEquals(4, amount);
+        assertEquals("Pineapple", item.getFoodName());
 
     }
 
@@ -71,20 +96,20 @@ public class ShopeeTest extends ApplicationTest {
 
     @Test
     public void testNewAmountWhenAddingSameFood() {
-        ShopeeList s = testUser.getShopeeList();
-
         clickOn("#newFood").write("Water");
         clickOn("#amountNewFood").write("2");
         clickOn("#addFood");
+
+        ShopeeList list1 = this.dataAccess.getAllUsers().get(0).getShopeeList("testlist");
+        assertEquals(2, list1.getFood("Water").getFoodAmount());
 
         
         clickOn("#newFood").write("Water");
         clickOn("#amountNewFood").write("19");
         clickOn("#addFood");
 
-        //size = 2 because i have already added 'apple' to the list
-        assertEquals(2, s.getShopList().size());
-        assertEquals(19, s.getShopList().get(1).getFoodAmount());
+        ShopeeList list2 = this.dataAccess.getAllUsers().get(0).getShopeeList("testlist");
+        assertEquals(19, list2.getFood("Water").getFoodAmount());
     }
 
     /**
@@ -92,71 +117,62 @@ public class ShopeeTest extends ApplicationTest {
      * in the bought shopping list if a food item is marked as bought
      */
 
-    @Test
-    public void testMarkAsBoughtButtonClick() {
-        ShopeeList s = testUser.getShopeeList();
+    // @Test
+    // public void testMarkAsBoughtButtonClick() {
+    //     ShopeeList s = testUser.getShopeeList();
 
-        clickOn("#newFood").write("Chocolate");
-        clickOn("#amountNewFood").write("3");
-        clickOn("#addFood");
+    //     clickOn("#newFood").write("Chocolate");
+    //     clickOn("#amountNewFood").write("3");
+    //     clickOn("#addFood");
 
-        // Simulate user interactions to mark the item as bought
-        clickOn("#shoppingListView").type(KeyCode.DOWN); // Select the added item
-        clickOn("#foodBought");
-        assertEquals(1, s.getBoughtList().size());
-        assertEquals(1, s.getShopList().size());
-        assertEquals("Chocolate", s.getBoughtList().get(0).getFoodName());
-    }
+    //     // Simulate user interactions to mark the item as bought
+    //     clickOn("#shoppingListView").type(KeyCode.DOWN); // Select the added item
+    //     clickOn("#foodBought");
+    //     assertEquals(1, s.getBoughtList().size());
+    //     assertEquals(1, s.getShopList().size());
+    //     assertEquals("Chocolate", s.getBoughtList().get(0).getFoodName());
+    // }
 
-    /**
-     * Test method tests if a food item is removed from the list 
-     */
+    // /**
+    //  * Test method tests if a food item is removed from the list 
+    //  */
     
-    @Test
-    public void testRemoveItem() {
-        ShopeeList s = testUser.getShopeeList();
-        // Simulate user interactions to add an item
-        clickOn("#newFood").write("Cheese");
-        clickOn("#amountNewFood").write("2");
-        clickOn("#addFood");
+    // @Test
+    // public void testRemoveItem() {
+    //     ShopeeList s = testUser.getShopeeList();
+    //     // Simulate user interactions to add an item
+    //     clickOn("#newFood").write("Cheese");
+    //     clickOn("#amountNewFood").write("2");
+    //     clickOn("#addFood");
 
-        //Test to see if it gets added to the list
-        assertEquals(2, s.getShopList().size());
-        assertEquals("Cheese", s.getShopList().get(1).getFoodName());
+    //     //Test to see if it gets added to the list
+    //     assertEquals(2, s.getShopList().size());
+    //     assertEquals("Cheese", s.getShopList().get(1).getFoodName());
 
-        // Simulate user interactions to remove the item
-        clickOn("#shoppingListView").type(KeyCode.DOWN); // Select the added item
-        clickOn("#removeFood");
+    //     // Simulate user interactions to remove the item
+    //     clickOn("#shoppingListView").type(KeyCode.DOWN); // Select the added item
+    //     clickOn("#removeFood");
 
-        assertEquals(1, s.getShopList().size());
-    }
+    //     assertEquals(1, s.getShopList().size());
+    // }
 
-    /**
-     * Tests if the logIn.fxml is shown when log out button is clicked
-     */
-
-    @Test
-    public void testLogOut() {
-        clickOn("#logOut");
-        
-    }
-
+    
 
     /**
      * Method to create a user which is used in all tests
      */
-    public User exampleUser(){
-        FileHandeler handler = new FileHandeler();
-        handler.clearFileContent();
-        this.testUser = new User("johan@ntnu.no", "Johan123@");
-        ShopeeList sList = new ShopeeList("Test");
-        sList.addFoodShopList("Apple", 4);
-        // sList.addFoodShopList("Cheese", 2);
-        // sList.addFoodShopList("Bread", 1);
-        testUser.setShopeeList(sList);
-        return testUser; 
+    // public User exampleUser(){
+    //     FileHandeler handler = new FileHandeler();
+    //     handler.clearFileContent();
+    //     this.testUser = new User("johan@ntnu.no", "Johan123@");
+    //     ShopeeList sList = new ShopeeList("Test");
+    //     sList.addFoodShopList("Apple", 4);
+    //     // sList.addFoodShopList("Cheese", 2);
+    //     // sList.addFoodShopList("Bread", 1);
+    //     testUser.setShopeeList(sList);
+    //     return testUser; 
 
-     }
+    //  }
 
 
 
