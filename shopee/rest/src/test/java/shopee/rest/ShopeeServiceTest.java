bbe6b6e87b4630f337c2e97dd506e827d7f22798
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-06 18:05:47 +0100

    Created a test for addUser, need to implement a helper method in FileHandeler later

--- shopee/rest/src/test/java/shopee/rest/ShopeeServiceTest.java
+++ shopee/rest/src/test/java/shopee/rest/ShopeeServiceTest.java
@@ -1,14 +1,20 @@
 package shopee.rest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import shopee.core.ShopeeList;
 import shopee.core.User;
+import shopee.json.FileHandeler; 
 
 
 public class ShopeeServiceTest {
-     private ShopeeUserService shopeeService;
+    private ShopeeUserService shopeeService;
+    private FileHandeler fileHandler; 
     private List<User> allUsers; 
     private User exampleUser; 
     private ShopeeList exampleList; 
@@ -23,14 +29,21 @@ public class ShopeeServiceTest {
         try {
             shopeeService = new ShopeeUserService();
             shopeeService.setAllUsers(ShopeeUserService.createInitialUser());
+            fileHandler = new FileHandeler();
         } catch (Exception e) {
             e.printStackTrace();
         }
      
     }
 
+    @Test 
+    public void addUser(){
+        User testUser = new User("Richard@gmail.com", "Password123!");
+        User testUser2 = new User("Daniel@outlook.no", "HarryP123@");
+        shopeeService.addUser(fileHandler.fromUserToString(testUser));
+        assertTrue(shopeeService.getAllUsers().contains(testUser), "The user was not added correctly");
+        assertFalse(shopeeService.getAllUsers().contains(testUser2), "User was added even though it should't");
+    }
     
     
-
-
 }
