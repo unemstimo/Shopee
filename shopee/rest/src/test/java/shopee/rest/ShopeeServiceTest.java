4a821c5b44b8a526a5146151f693f68b41af82f9
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-06 17:47:31 +0100

    Added a BeforeEach method that resets the list between test

--- shopee/rest/src/test/java/shopee/rest/ShopeeServiceTest.java
+++ shopee/rest/src/test/java/shopee/rest/ShopeeServiceTest.java
@@ -1,3 +1,36 @@
-public class ServiceTest {
+package shopee.rest;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+
+import shopee.core.ShopeeList;
+import shopee.core.User;
+
+
+public class ShopeeServiceTest {
+     private ShopeeUserService shopeeService;
+    private List<User> allUsers; 
+    private User exampleUser; 
+    private ShopeeList exampleList; 
+
+
+    /* 
+     * Sets up a service and uses createIntialUser to reset the 
+     * user info between each test
+     */
+    @BeforeEach
+    public void setUp() {
+        try {
+            shopeeService = new ShopeeUserService();
+            shopeeService.setAllUsers(ShopeeUserService.createInitialUser());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+     
+    }
+
     
+    
+
+
 }
