12b1ce1ebb20bba5b3c5c5eba6567926b66d3d76
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-14 18:39:06 +0100

    Fixed checkstyle errors for core tests

--- shopee/core/src/test/java/shopee/json/FileHandlerTest.java
+++ shopee/core/src/test/java/shopee/json/FileHandlerTest.java
@@ -2,15 +2,19 @@ package shopee.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import shopee.core.User;
 import shopee.core.ShopeeList; 
+import shopee.core.User;
 
 
+/**
+ * Test for file handling to and from json files.
+ */
 public class FileHandlerTest {
 
     private FileHandeler fileHandler = new FileHandeler();
@@ -28,21 +32,23 @@ public class FileHandlerTest {
 
     /**
      * Test the writeToFile(User) method and jsonToObj()
-     * Checks if new login info i saved properly plus shopping list info. Sees if the added fooditems 
+     * Checks if new login info i saved properly plus shopping 
+     * list info. Sees if the added fooditems 
      * match with what we write when creating a list.
-     * @throws FileNotFoundException
+     * 
      */
     @Test
     public void testWriteAndReadUserToFile() throws FileNotFoundException {
-        // Create a User object for testing
-        User testUser = new User("Johan@gmail.no", "johan123@");
+        
         // Add a shopeelist to User 
         ShopeeList newList = new ShopeeList("Onsdag");
-        newList.addFoodShopList("Chips",7);
+        newList.addFoodShopList("Chips", 7);
         newList.addFoodBoughtList(newList.getFood("Chips"));
         newList.addFoodShopList("Apple", 4);
         newList.addFoodShopList("Bread", 1);
 
+        // Create a User object for testing
+        User testUser = new User("Johan@gmail.no", "johan123@");
         testUser.addShopeeList(newList);
 
         // Write the User object to the test file
@@ -57,28 +63,25 @@ public class FileHandlerTest {
         assertEquals(testUser.getUsername(), userList.get(0).getUsername());
         assertEquals(testUser.getPassword(), userList.get(0).getPassword());
         assertEquals(2, testUser.getShopeeList(newList.getListName()).getShopList().size());
-        assertEquals("Apple", testUser.getShopeeList(newList.getListName()).getShopList().get(0).getFoodName());
-        assertEquals(1, testUser.getShopeeList(newList.getListName()).getShopList().get(1).getFoodAmount());
-        assertEquals("Chips", testUser.getShopeeList(newList.getListName()).getBoughtItem(0).getFoodName());
+        assertEquals("Apple", testUser.getShopeeList(newList.getListName())
+        .getShopList().get(0).getFoodName());
+        assertEquals(1, testUser.getShopeeList(newList.getListName())
+        .getShopList().get(1).getFoodAmount());
+        assertEquals("Chips", testUser.getShopeeList(newList.getListName())
+        .getBoughtItem(0).getFoodName());
     }
 
-    /**
-     * Test the writeToFile(User) method further.
-     * Checks if the amount updates when adding the same ingredient to the list 
-     * it should not be duplicates and only the last given amount should be saved.
-     * @throws FileNotFoundException
-     */
     @Test
     public void testUpdateUserInFile() throws FileNotFoundException {
         fileHandler.clearFileContent();
         // Create a User object for testing
         User testUser = new User("oskar@gmail.com", "Password12!");
-        User testUser2 = new User("Hanna@gmail.com", "Henryd12!");
         ShopeeList newList = new ShopeeList("Onsdag");
-        newList.addFoodShopList("Chips",7);
-        newList.addFoodShopList("Chips",3);
+        newList.addFoodShopList("Chips", 7);
+        newList.addFoodShopList("Chips", 3);
         testUser.addShopeeList(newList);
 
+        User testUser2 = new User("Hanna@gmail.com", "Henryd12!");
         // Write the User object to the test file
         fileHandler.writeToFile(testUser);
         fileHandler.writeToFile(testUser2);
@@ -94,13 +97,15 @@ public class FileHandlerTest {
         assertNotNull(userList);
         assertEquals(2, userList.size());
         assertEquals("NewPwd4$", userList.get(0).getPassword());
-        assertEquals(3, testUser.getShopeeList(newList.getListName()).getShopList().get(0).getFoodAmount());
+        assertEquals(3, testUser.getShopeeList(newList.getListName())
+        .getShopList().get(0).getFoodAmount());
     }
 
     /**
      * Test the clearFileContent() helper method 
-     * Checks if all stored data is removed from the file, such that an empty file can be used in other methods.
-     * @throws FileNotFoundException
+     * Checks if all stored data is removed from the file, ¨
+     * such that an empty file can be used in other methods.
+     * 
      */
     @Test
     public void testClearFileContent() throws FileNotFoundException {
@@ -120,22 +125,25 @@ public class FileHandlerTest {
         assertNotNull(userList);
         assertEquals(0, userList.size());
     }
-
-
    
-
+    /**
+     * Test the clearFileContent() helper method 
+     * Checks if all stored data is removed from the file, 
+     * such that an empty file can be used in other methods.
+     */
     public User setUpTestuser() {
-        User testUser = new User("test@test.no", "test1234@");
         
         ShopeeList newList = new ShopeeList("Middag");
-        newList.addFoodShopList("Chips",7);
+        newList.addFoodShopList("Chips", 7);
         newList.addFoodShopList("Apple", 4);
         newList.addFoodShopList("Bread", 1);
 
         ShopeeList newlist2 = new ShopeeList("Lunsj");
-        newlist2.addFoodShopList("Tomat",7);
+        newlist2.addFoodShopList("Tomat", 7);
         newlist2.addFoodShopList("Fisk", 4);
         newlist2.addFoodShopList("Mat", 1);
+
+        User testUser = new User("test@test.no", "test1234@");
 
         testUser.addShopeeList(newList);
         testUser.addShopeeList(newlist2);
