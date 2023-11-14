0571b4f2d513fd35dcac1f76602f66914fbe3eeb
johanleg@stud.ntnu.no <johanlegreid@hotmail.com>
2023-11-14 09:34:29 +0100

    Removed unused imports in ApplicationTest and moved service field inside the test

--- shopee/rest/src/test/java/shopee/rest/ShopeeApplicationTest.java
+++ shopee/rest/src/test/java/shopee/rest/ShopeeApplicationTest.java
@@ -4,21 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import shopee.core.User;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -27,11 +23,12 @@ public class ShopeeApplicationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-    private ShopeeUserService service = new ShopeeUserService();
+ 
     
      
      @Test
     public void contextLoads() throws FileNotFoundException {
+        ShopeeUserService service = new ShopeeUserService();
         service.setAllUsers(ShopeeUserService.createInitialUser());
 
         // Make a request to the root URL of the application.
